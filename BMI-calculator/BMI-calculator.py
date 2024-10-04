b18dfb8bb52ff98d96a362ba20486c368e8812f9
prabhatbhusal <prabhatbhusal777@gmail.com>
2024-10-04 18:12:08 +0545

    Create BMI-calculator.py

---
+++ BMI-calculator/BMI-calculator.py
@@ -0,0 +1,8 @@
+# 1st input: enter height in meters e.g: 1.65
+height = input()
+# 2nd input: enter weight in kilograms e.g: 72
+weight = input()
+h=float(height)
+w=int(weight)
+BMI=w/(h**2)
+print(int(BMI))
