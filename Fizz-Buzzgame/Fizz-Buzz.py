c87d8ab0d421fc6a9c3c3673c8b69346daf91f54
prabhatbhusal <prabhatbhusal777@gmail.com>
2024-10-06 17:00:08 +0545

    Create Fizz-Buzz.py

---
+++ Fizz-Buzzgame/Fizz-Buzz.py
@@ -0,0 +1,11 @@
+# Fizz Bzz Game
+for number in range(1,101):
+  if(number%5==0 and number%3==0):
+    print("FizzBuzz")
+  elif(number%5==0):
+    print("Buzz")
+  elif(number%3==0):
+    print("Fizz")
+  else:
+    print(number)
+  
