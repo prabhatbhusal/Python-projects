d0139835de37739658d0b474cbb920185698e7d3
Prabhat Bhusal <prabhatbhusal777@gmail.com>
2024-10-14 19:39:16 +0545

    Add files via upload

---
+++ higher-lowegame/higherlowegame.py
@@ -0,0 +1,12 @@
+from art import logo,vs
+from gamedata import data
+import random
+print(logo)
+#1
+datas={}
+datas=random.choice(data)
+sample={}
+sample=random.choice(data)
+print(datas)
+print(vs)
+print(sample)
